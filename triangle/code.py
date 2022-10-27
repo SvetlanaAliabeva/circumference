bbd8be6c935e5132cfaeec324eaeaf5053317765
SvetlanaAliabeva <112935755+SvetlanaAliabeva@users.noreply.github.com>
2022-10-27 15:53:06 +0900

    Update code.py

--- triangle/code.py
+++ triangle/code.py
@@ -1,1 +1,8 @@
+a, b, c = 10, 10, 10
 
+def triangle_perimeter(side1=a, side2=b, side3=c):
+  return side1 + side2 + side3
+  
+def triangle_area(side1=a, side2=b, side3=c):
+  p = (side1 + side2 + side3) / 2
+  return p * (p - side1) * (p - side2) * (p - side3)
